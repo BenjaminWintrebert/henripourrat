2acef71a019a8fd158e400ec2b849054ccecdcfa
WintrebertBenjaminWeb2 <benjamin.wintrebert@laposte.net>
2016-03-30 12:47:15 +0200

    Ajout du bloc parallax sur l'accueil

--- accueil.php
+++ accueil.php
@@ -5,6 +5,17 @@ Template Name: Accueil
 ?><?php
 
 get_header(); ?>
+<section id="accueil_hp">
+	<div id="img_parallax"></div>
+	<div id="hp_container">
+		<h1>Henri Pourrat</h1>
+		<hr>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a suscipit nibh. Sed vehicula enim eget maximus accumsan. In lobortis enim ut neque sollicitudin facilisis. Curabitur finibus ultrices nunc eu accumsan. Donec at nisl et sapien aliquet dignissim sed mattis est.
+		</p>
+		<div class='btn'>Biographie</div>
+	</div>
+</section>
 <section id="accueil_actu">
     <?php query_posts('showposts=3'); if (have_posts()) : while (have_posts()) : the_post(); ?>
         <div class="article_container">
