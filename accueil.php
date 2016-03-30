d215c690d1ddc6f64439fe3e5f5dcc70ac4eb856
bastien.mottier@gmail.com <bastien.mottier@gmail.com>
2016-03-30 12:58:48 +0200

    Affichage 3 derniers articles

--- accueil.php
+++ accueil.php
@@ -5,14 +5,20 @@ Template Name: Accueil
 ?><?php
 
 get_header(); ?>
-<section id="accueil_actu" class="clearfix">
+<section id="accueil_actu" class="wrapper clearfix">
     <h1>Actualités</h1>
     <?php query_posts('showposts=3'); if (have_posts()) : while (have_posts()) : the_post(); ?>
         <div class="article_container">
-            <?php the_thumb_url() ?>
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-            <?php the_date(); ?>
-            <?php the_content(); ?>
+            <div class="left thumb_article" style="background-image: url('<?php the_thumb_url() ?>')">
+
+            </div>
+            <div class="left">
+                <div class="text">
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    <?php the_date(); ?>
+                    <?php the_content(); ?>
+                </div>
+            </div>
         </div>
     <?php endwhile;?>
      <?php endif; wp_reset_query(); ?>
